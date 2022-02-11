d6ec45e13f685e7e0a715c234c5662cd9447a12d
lucas.pena <lucasbpena2003@gmail.com>
2022-02-11 14:28:46 -0300

    Update main.c
    nada demais
    [ ] permutar PERMUTAR tem que colocar as lista pra permutar eu nao sei como fAZ

--- codigo/main.c
+++ codigo/main.c
@@ -27,7 +27,7 @@
 
 /* Declarações */
 
-void leitura_arquivos();
+void leitura_arquivos(int num_teste, char* pnome_arquivo);
 void nome_arquivo_testes(int num_teste, char* pnome_arquivo);
 
 
@@ -38,30 +38,10 @@ int main()
 {
     // Declarações
     clock_t start, end;  // cock_t YEP COCK (Clock)
-
-    start = clock();
-    leitura_arquivos();
-    end = clock();
-    
-    double tempo_gasto = ((double) (end - start) / CLOCKS_PER_SEC);  // Registra tempo gasto
-}
-
-// Função para leitura dos arquivos
-void leitura_arquivos()
-{
-    // Declarações
-    int N;  // Quantidade de cidades, linha 1
-    int Qv;  // Capacidade do caminhão, linha 2
-    int D;  // Demanda de cada cidade, linha 3, implementada como vetor
-    int Ci, Cj, Dij;  // Cidade i, Cidade j, Distância entre cidades i e j, linhas seguintes
     int num_teste = 0;  // Número de teste dos arquivos
-    
-    FILE* input;  // Arquivo de entrada
-    FILE* output;  // Arquivo de saída (possivelmente necessario)
-    
- 
     char nome_arquivo[MAX_CHAR_LIMIT];
     char *pnome_arquivo = nome_arquivo;
+
     printf("Escolha um numero de arquivo de teste (1 a 7): ");
     scanf("%d", &num_teste);
     if (num_teste < PRIMEIRO_ARQUIVO_TESTE || num_teste > NUM_ARQUIVOS_TESTE)
@@ -70,7 +50,30 @@ void leitura_arquivos()
         exit(TRUE);
     }
     nome_arquivo_testes(num_teste, pnome_arquivo);  
-    input = fopen(nome_arquivo, "r");  // Abre o arquivo para leitura
+
+    start = clock();
+    leitura_arquivos(num_teste, pnome_arquivo);
+    end = clock();
+    
+    double tempo_gasto = ((double) (end - start) / CLOCKS_PER_SEC);  // Registra tempo gasto
+    printf("Tempo gasto para permutação: %lf", tempo_gasto);
+
+    return 0;
+}
+
+// Função para leitura dos arquivos
+void leitura_arquivos(int num_teste, char* pnome_arquivo)
+{
+    // Declarações
+    int N;  // Quantidade de cidades, linha 1
+    int Qv;  // Capacidade do caminhão, linha 2
+    int D;  // Demanda de cada cidade, linha 3, implementada como vetor
+    int Ci, Cj, Dij;  // Cidade i, Cidade j, Distância entre cidades i e j, linhas seguintes
+    
+    FILE* input;  // Arquivo de entrada
+    FILE* output;  // Arquivo de saída (possivelmente necessario)
+    
+    input = fopen(pnome_arquivo, "r");  // Abre o arquivo para leitura
 
     /* 
      * 
@@ -91,7 +94,8 @@ void leitura_arquivos()
     tam = N-1;
     vetDem = criar_vetDem(tam);
     
-    for (int i=0;i<tam;i++){
+    for (int i=0;i<tam;i++)
+    {
         fscanf(input, "%d", &D);
         preencher_vetDem(vetDem,i,D);
     }
@@ -100,37 +104,35 @@ void leitura_arquivos()
     Tdistancias *matDist;
     matDist = criar_matDist(N);
     
-        int num_linhas;
+    int num_linhas;
     
-    if (N == 3){
-        for (int k = 0; k <= N; k++){
+    if (N == 3)
+    {
+        for (int k = 0; k <= N; k++)
+        {
             fscanf(input, "%d %d %d", &Ci, &Cj, &Dij);
             preencher_matDist(matDist, Ci, Cj, Dij);
         }
-        else{
+        else
+        {
             num_linhas = N + (N-1);
-            for (int k = 0; k <= num_linhas; k++){
+            for (int k = 0; k <= num_linhas; k++)
+            {
                 fscanf(input, "%d %d %d", &Ci, &Cj, &Dij);
                 preencher_matDist(matDist, Ci, Cj, Dij);
             }
         }
     }
     
-    fclose(input);
-        
+    fclose(input);      
 
-    printf("Num teste = %d\n", num_teste);
+    printf("Teste numero %d\n", num_teste);
     
-    printf("Numero Cidades = %d\n",N);
-    printf("Capacidade Caminhao = %d\n",Qv);
-    
+    printf("Numero de Cidades = %d\n", N);
+    printf("Capacidade Caminhao = %d\n", Qv);
     exibir_vetDem(vetDem);
     exibir_matDist(matDist);
-        // exibir_tempogasto(tempo_gasto)
-        //
-        
-    printf("\n");
-      
+
     }
 
 }
